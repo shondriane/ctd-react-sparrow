e6f6f58f6255e596081085ef42d4bb615e70ff2d
shondriane <shondriane.wise@yahoo.com>
2022-05-09 08:23:24 -0400

    Async data and add local state

--- src/App.js
+++ src/App.js
@@ -12,46 +12,66 @@ the todolist it's returning the initial state
 (what is currently saved in the local storage even if it's an empty [] string)
 and returning that list*/
 
-const useSemiPersistentState = (initialState) => {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || initialState
-  );
-  React.useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
-  console.log(useSemiPersistentState);
-  return [todoList, setTodoList];
-};
+ /* Custom-hook keeps  component's state in synch with local browser's storage
+to clear the list set useSemiPersistentStae to an empty string []*/
 
 function App() {
-  /* Custom-hook keeps  component's state in synch with local browser's storage
-to clear the list set useSemiPersistentStae to an empty string []*/
-  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList");
+ 
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList") || [])
+  );
+  const [isLoading, setIsLoading]= React.useState(true);
+
+  /* Saves TodoList into Local Storage */
+  React.useEffect(() => {
+
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+
+  }, [todoList]);
+  console.log(todoList);
   
-  /* Adds the new todo List item to the existing list */
+/* New useEffect that sets loading for 2 seconds before rendering the saved todoList or empty array*/
+  React.useEffect (()=>{
+    setIsLoading(true);
+
+    new Promise (resolve =>
+      setTimeout(
+        () => resolve({data: {todoList:JSON.parse(localStorage.getItem("savedTodoList") || [])}}),
+        2000
+      )
+      ).then(result => {
+        setTodoList(result.data.todoList);
+        setIsLoading(false);
+      });
+    
+},[]);
+
+  
+/* Adds the new todo List item to the existing list */
+
   const addTodo = (newTodo) => {
     console.log([...todoList, newTodo]);
-    /*localStorage.setItem('todoList',[...todoList,newTodo]);*/
     return setTodoList([...todoList, newTodo]);
-    
   };
+  
   /*removes todoList item*/
 
-  const removeTodo = (id)=>{
-    const newList = todoList.filter (
-    (item)=>item.id !== id
-    );
+  const removeTodo = (id) => {
+    const newList = todoList.filter((item) => item.id !== id);
     setTodoList(newList);
   };
   return (
     /*fragment wrapps sibling elements into a single top-level
     element*/
+
     <React.Fragment>
       <h1> Todo List</h1>
       <hr />
 
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo ={removeTodo} />
+      {isLoading ?(
+        <p>Loading ....</p>):(
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}
+      
     </React.Fragment>
   );
 }
